5c3f4b7064b0e9c4b041c1dfac465e6cbbaeb242
jkcachero <johnkarlo.315@gmail.com>
2025-05-21 17:41:02 +0800

    Authentication routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Tweet;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'email' => 'johnkarlo.315@gmail.com',
+            'name' => 'John Karlo',
+            'username' => 'johnkarlo'
+        ]);
+
         Tweet::factory(100)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Models\Tweet;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -47,3 +49,32 @@ Route::get('/users/{user}', function (User $user) {
 Route::get('/users/{user}/tweets', function (User $user) {
     return $user->tweets()->with('user:id,name,username,avatar')->latest()->paginate(10);
 });
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required', 'string'],
+        'device_name' => ['required','string'],
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['Invalid credentials'],
+        ]);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'user' => $user->only('id', 'name', 'email', 'username', 'avatar'),
+    ], 201);
+});
+
+ROute::post('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+
+    return response()->json('Logged out successfully.', 200);
+})->middleware('auth:sanctum');
